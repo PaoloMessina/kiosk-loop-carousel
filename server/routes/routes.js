d153899cc2ffb21a09c4408668f21481f446dbdb
Paolo Messina <phemt.latd@gmail.com>
2016-02-05 17:14:48 +0100

    bugfix
    add cors management

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,11 @@
 var appRouter = function(app) {
 
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
+
 	app.get("/getList", function(req, res) {
 		res.send("Hello World");
 	});

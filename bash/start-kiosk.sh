2edf7316cb5354ce670546fa6415d995499c0411
Paolo Messina <phemt.latd@gmail.com>
2016-02-05 15:32:23 +0100

    add delay to chrome start
    add delay to chrome start

--- bash/start-kiosk.sh
+++ bash/start-kiosk.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-timeout 5 chromium-browser --disable-web-security --kiosk http://localhost:9000 --incognito --noerrdialog --test-type
+sleep 3 
+chromium-browser --disable-web-security --kiosk http://localhost:9000 --incognito --noerrdialog --test-type
